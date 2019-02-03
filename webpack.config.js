ba02016b06c86f8b873f622f23e7e1b33fd09506
shin-kinoshita <kinoshita116@gmail.com>
2019-02-03 17:03:51 +0900

    modify to run as electron application

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,45 @@
 /*global require, module, __dirname, process */
+// ===================================================================================
+//                                                                          Definition
+//                                                                          ==========
 const path = require('path');
-module.exports = {
-	entry: './src/start',
+const rendererConfig = {
+	target:'electron-renderer',
+	entry: {
+		'renderer/start': './src/renderer/start'
+	}
+};
+const mainConfig = {
+	target:'electron-main',
+	entry: {
+		'main/index': './src/main/index'
+	},
 	devtool: 'source-map',
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'site/')
 	}
 };
+const commonConfig = {
+	node: {
+		__dirname: false,
+		__filename: false,
+	},
+	devtool: 'source-map',
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'site/')
+	}
+};
+
+// ===================================================================================
+//                                                                          Initialize
+//                                                                          ==========
+const concatCommonConfig = (config) => {
+	Object.keys(commonConfig).forEach(key => {
+		config[key] = commonConfig[key];
+	});
+};
+concatCommonConfig(rendererConfig);
+concatCommonConfig(mainConfig);
+module.exports = [ rendererConfig, mainConfig ];
